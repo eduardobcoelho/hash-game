af59c0fe76e22ffce4b4ec7ec8dd83f48c26456d
eduardobcoelho <eduardobcoelho27@outlook.com>
2020-06-25 06:57:53 -0300

    reset board function

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+var i = 0;
+
 var tokken = "X"
+
+var posX;
+
+var posY;
 
 var boardFields = document.querySelectorAll(".space");
 
 var boardArray = [
+
+    [boardFields[0], boardFields[1], boardFields[2]],
+    [boardFields[3], boardFields[4], boardFields[5]],
+    [boardFields[6], boardFields[7], boardFields[8]]
+
+]
+
+var boardTokken = [
 
     [null, null, null],
     [null, null, null],
@@ -10,17 +24,27 @@ var boardArray = [
 
 ]
 
-function showTokken(fieldClicked) {
+function assignTokken(fieldClicked) {
 
     fieldClicked.insertAdjacentHTML("beforeend", "<p class='tokken font-class'>" + tokken + "</p>");
 
     let fieldClasses = fieldClicked.classList;
 
-    let posX = fieldClasses.length - 2;
+    if (fieldClasses.length == 2) {
 
-    let posY = fieldClasses.length - 1;
+        posX = fieldClasses[fieldClasses.length - 1];
 
-    boardArray[posX][posY] = tokken;
+        posY = fieldClasses[fieldClasses.length - 1];
+
+    } else {
+
+        posX = fieldClasses[fieldClasses.length - 2];
+
+        posY = fieldClasses[fieldClasses.length - 1];
+
+    }
+
+    boardTokken[posX][posY] = tokken;
 
     if (tokken == "X") {
 
@@ -30,6 +54,27 @@ function showTokken(fieldClicked) {
 
         tokken = "X";
 
+    }
+
+}
+
+
+function resetBoard() {
+
+    let tokkens = document.querySelectorAll(".tokken");
+
+    for (i = 0; i < tokkens.length; i++) {
+
+        tokkens[i].parentNode.removeChild(tokkens[i]);
+
+    }
+
+    for (i = 0; i < 3; i++) {
+        for (var j = 0; j < 3; j++) {
+
+            boardTokken[i][j] = null;
+
+        }
     }
 
 }
